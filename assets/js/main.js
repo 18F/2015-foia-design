858127a39d30bbc0dc8c20b6200c8ff287721014
Eric Mill <eric.mill@gsa.gov>
2014-08-13 15:04:36 -0400

    moved core to port 5000

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 // global variables for the app
 
 Env = {
-  core: "http://ec2-54-210-43-35.compute-1.amazonaws.com",
+  core: "http://ec2-54-210-43-35.compute-1.amazonaws.com:5000",
   search: "http://ec2-54-210-43-35.compute-1.amazonaws.com:8080"
 }
